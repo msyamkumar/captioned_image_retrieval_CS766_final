814238256c64f4c8b0fd262d135e42ec41184483
Jia-Shen Boon <jiashen@gmail.com>
2015-04-19 21:33:12 -0500

    Analyse words by kurtosis of TFIDF

--- baselineCaptions.m
+++ baselineCaptions.m
@@ -3,27 +3,28 @@
 
 %% Get captions from each image
 
-fid = fopen('data/Flickr8k_text/Flickr8k.lemma.token.txt');
-concatCaptions = textscan(fid, '%s', 'delimiter', '\n');
-fclose(fid);
-concatCaptions = concatCaptions{1}(1:10000);
-numLines = numel(concatCaptions);
-for i = 1 : numLines
-    line = upper(concatCaptions{i});
-    tokens = strsplit(line, '\t');
-    concatCaptions{i} = tokens{end};
+numCaptions = 10000;
+captionMap = loadCaptionMap();
+numImages = numel(captionMap.keys);
+imageList = captionMap.keys;
+
+% Change all captions to uppercase
+for i = 1 : numImages
+    captionMap(imageList{i}) = upper(captionMap(imageList{i}));
 end
 
+concatCaptions = captionMap.values;
+
 % Build vocabulary so that we can build bag of words
-vocab = unique(strsplit(strjoin(concatCaptions')));  % strjoin adds a space delimiter
-vocabSize = numel(vocab);
+fullVocab = unique(strsplit(strjoin(concatCaptions)));  % strjoin adds a space delimiter
+fullVocabSize = numel(fullVocab);
 
 % Build non-Boolean BoW for each caption
-fprintf('Building %i bags of words... ', numLines); tic;
-bagsOfWords = zeros(numLines, vocabSize);
-for i = 1 : numLines
-    tokens = strsplit(concatCaptions{i});
-    bagsOfWords(i, :) = countmember(vocab, tokens);
+fprintf('Building %i bags of words... ', numImages); tic;
+bagsOfWords = zeros(numImages, fullVocabSize);
+for i = 1 : numImages
+    tokens = strsplit(captionMap(imageList{i}));
+    bagsOfWords(i, :) = countmember(fullVocab, tokens);
 end
 toc;
 
@@ -32,12 +33,51 @@ toc;
 kurtosises = kurtosis(bagsOfWords);
 [~, sortInds] = sort(kurtosises, 'descend');  %# Sort the values in
                                                   %#   descending order
-%%
+
 numRanks = 5;  % Top N/ bottom N words
                                              
 maxInds = sortInds(1:numRanks); 
 minInds = sortInds(end - numRanks + 1: end);
 fprintf('Top most skewed words in decreasing skewness\n');
-disp(vocab(maxInds)');
+disp(fullVocab(maxInds)');
 fprintf('Least skewed words in increasing skewness\n');
-disp(vocab(fliplr(minInds))');
+disp(fullVocab(fliplr(minInds))');
+
+%% Analyze document frequency of words
+
+docFrequencies = sum(bagsOfWords > 0);
+
+[~, sortInds] = sort(docFrequencies, 'descend');  %# Sort the values in
+                                                  %#   descending order
+numRanks = 5;  % Top N/ bottom N words
+                                             
+maxInds = sortInds(1:numRanks); 
+minInds = sortInds(end - numRanks + 1: end);
+midInds = sortInds(floor(fullVocabSize / 2 - numRanks / 2) : floor(fullVocabSize / 2 - numRanks / 2) + numRanks - 1);
+fprintf('Words with highest doc frequency\n');
+disp(fullVocab(maxInds)');
+fprintf('Words with lowest doc frequency\n');
+disp(fullVocab(fliplr(minInds))');
+fprintf('Words with middle doc frequency\n');
+disp(fullVocab(fliplr(midInds))');
+
+
+%% Analyze tdidfs of words
+
+tfidfs = bagsOfWords;
+for i = 1 : numImages
+    tfidfs(i, :) = tfidfs(i, :) ./ docFrequencies;
+end
+                           
+kurtosises = kurtosis(tfidfs);
+[~, sortInds] = sort(kurtosises, 'descend');  %# Sort the values in descending order
+
+numRanks = 5;  % Top N/ bottom N words
+                                             
+maxInds = sortInds(1:numRanks); 
+minInds = sortInds(end - numRanks + 1: end);
+fprintf('Top most skewed words in decreasing skewness\n');
+disp(fullVocab(maxInds)');
+fprintf('Least skewed words in increasing skewness\n');
+disp(fullVocab(fliplr(minInds))');
+%%
