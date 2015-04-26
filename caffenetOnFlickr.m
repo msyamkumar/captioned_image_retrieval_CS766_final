14bf6e3c24a29de4386685b1ec22a0447780ebee
Jia-Shen Boon <jiashen@gmail.com>
2015-04-26 18:43:54 -0500

    Change caffenet function to script

--- caffenetOnFlickr.m
+++ caffenetOnFlickr.m
@@ -1,8 +1,14 @@
-function [scores, maxlabel] = matcaffe_demo(im, use_gpu)
-% scores = matcaffe_demo(im, use_gpu)
-%
-% Demo of the matlab wrapper using the ILSVRC network.
-%
+% function [scores, maxlabel] = matcaffe_demo(im, use_gpu)
+
+%% Apply CaffeNet to Flickr8k data
+% 
+% We want to feed Flicker8k data to a pre-trained model CaffeNet. The
+% motivation is to get a lower-dimensional representation of arbitrary
+% (including arbitrarily-sized) images.
+% 
+% We will remove the last soft-max layer of CaffeNet
+
+
 % input
 %   im       color image as uint8 HxWx3
 %   use_gpu  1 to use the GPU, 0 to use the CPU
@@ -53,9 +59,11 @@ else
   matcaffe_init();
 end
 
-if nargin < 1
-  % For demo purposes we will use the peppers image
-  im = imread('peppers.png');
+% For demo purposes we will use the peppers image
+im_filename = 'cameraman.tif';
+im = imread(im_filename);
+if size(im, 3) == 1
+    im = repmat(im, [1, 1, 3]);
 end
 
 % prepare oversampled input
@@ -68,43 +76,16 @@ toc;
 % scores are now Width x Height x Channels x Num
 tic;
 scores = caffe('forward', input_data);
+size(scores)
 toc;
 
 scores = scores{1};
 size(scores)
 scores = squeeze(scores);
+size(scores)
 scores = mean(scores,2);
 
 [~,maxlabel] = max(scores);
 
-% ------------------------------------------------------------------------
-function images = prepare_image(im)
-% ------------------------------------------------------------------------
-d = load('ilsvrc_2012_mean');
-IMAGE_MEAN = d.image_mean;
-IMAGE_DIM = 256;
-CROPPED_DIM = 227;
-
-% resize to fixed input size
-im = single(im);
-im = imresize(im, [IMAGE_DIM IMAGE_DIM], 'bilinear');
-% permute from RGB to BGR (IMAGE_MEAN is already BGR)
-im = im(:,:,[3 2 1]) - IMAGE_MEAN;
-
-% oversample (4 corners, center, and their x-axis flips)
-images = zeros(CROPPED_DIM, CROPPED_DIM, 3, 10, 'single');
-indices = [0 IMAGE_DIM-CROPPED_DIM] + 1;
-curr = 1;
-for i = indices
-  for j = indices
-    images(:, :, :, curr) = ...
-        permute(im(i:i+CROPPED_DIM-1, j:j+CROPPED_DIM-1, :), [2 1 3]);
-    images(:, :, :, curr+5) = images(end:-1:1, :, :, curr);
-    curr = curr + 1;
-  end
-end
-center = floor(indices(2) / 2)+1;
-images(:,:,:,5) = ...
-    permute(im(center:center+CROPPED_DIM-1,center:center+CROPPED_DIM-1,:), ...
-        [2 1 3]);
-images(:,:,:,10) = images(end:-1:1, :, :, curr);
+labels = loadSynsets();
+fprintf('Label of %s is %s\n', im_filename, labels{maxlabel});
