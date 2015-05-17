ad6b4320c879803fd9a022b2d0bb67bf49caee60
Jia-Shen Boon <jiashen@gmail.com>
2015-05-16 21:44:22 -0500

    Add things to my startup

--- js_startup.m
+++ js_startup.m
@@ -1,7 +1,10 @@
 % Start up file for Jia-Shen
 
 addpath('SpatialPyramid');
+addpath('combinedModel');
 addpath('caffenet');
+addpath('util');
 addpath(genpath('~/builds/caffe/'));
 addpath(genpath('data'));
 set(0, 'DefaultFigureWindowStyle', 'docked');
+iptsetpref('ImshowInitialMagnification', 'fit');  % remove imshow warnings
