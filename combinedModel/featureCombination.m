acb2024d5ac861969369d2c448046b7f55d13c11
msyamkum <meenakshi.psgit@gmail.com>
2015-05-16 19:16:27 -0500

    display results

--- combinedModel/featureCombination.m
+++ combinedModel/featureCombination.m
@@ -2,6 +2,8 @@ function [ output_args ] = featureCombination( ldaMatFile, NNMatFile )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
+%%Calculate kNN
+
 %LDA feature vector
 load(ldaMatFile);
 %DeepNet feature vector
@@ -34,6 +36,52 @@ combinedTest = [ldaTest nnTest];
 K = 3;
 fprintf('Running KNN where K = %i... ', K);tic;
 inds = knnsearch(combinedTrain, combinedTest, 'K', K);
+
+%% Load filenames of train/test set
+
+% Load image filenames of train/dev/test sets
+filenames = {'../data/Flickr8k_text/Flickr_8k.trainImages.txt', ...
+    '../data/Flickr8k_text/Flickr_8k.devImages.txt', ...
+    '../data/Flickr8k_text/Flickr_8k.testImages.txt'};
+for i = 1 : numel(filenames)
+    filename = filenames{i};
+    fid = fopen(filename);
+    if fid == -1; error('Error: %s cannot be opened', filename); end;
+    tmp = textscan(fid, '%s\n');
+    im_filename_sets{i} = tmp{1};
+end
+
+% Join train and dev sets together; test set is separate
+train_filenames = [im_filename_sets{1}];%; im_filename_sets{2}];
+test_filenames = im_filename_sets{end};
+
+% Remove filenames that weren't featurized (that's <10 examples)
+train_filenames = train_filenames(ismember(train_filenames, im_filenames));
+test_filenames = test_filenames(ismember(test_filenames, im_filenames));
+
+%% Display results
+
+if ~exist('do_display', 'var')  || do_display;
+
+    for ii = 1:20
+
+        query_filename = test_filenames{ii};
+
+        figure;
+        hold on;
+        subplot(1, K + 1, 1);
+        imshow(imread(query_filename));
+        title('Query');
+        
+        for ki = 1 : K
+            result_filename = train_filenames{inds(ii, ki)};
+            subplot(1, K + 1, ki + 1);
+            imshow(imread(result_filename));
+            title(sprintf('Top #%i result', ki));
+        end
+    end
+end
+    
 
 end
 
